113331e151c37d7333e09c50d5201043dc2eadd6
dream_chaser.421 <e14101171@gs.ncku.edu.tw>
2025-07-30 17:01:19 +0800

    update help()

--- eman.sh
+++ eman.sh
@@ -1,12 +1,79 @@
+#!/bin/bash
+
 help() {
     cat <<EOF
-    
-    eman check-verilator            : print the version of the first found Verilator (if there are multiple version of Verilator installed)
-    eman verilator-example          : compile and run the Verilator example(s)
-    eman change-verilator <VERSION> : change default Verilator to different version. If not installed, install it.
+Usage: $0 <command> [arguments]
 
-    eman c-compiler-version         : print the version of default C compiler and the version of GNU Make
-    eman c-compiler-example         : compile and run the C/C++ example(s)
-
+Commands:
+  check-verilator            : Print the version of the first found Verilator
+  verilator-example <DIR>    : Compile and run Verilator example(s) in specified directory
+  change-verilator <VERSION> : Change default Verilator to specified version (installs if not present)
+  c-compiler-version         : Print versions of default C compiler and GNU Make
+  c-compiler-example <DIR>   : Compile and run C/C++ example(s) in specified directory
 EOF
 }
+
+die() {
+    echo "Error: $1" >&2
+    help
+    exit 1
+}
+
+run_make_in_dir() {
+    local dir="$1"
+    if [ -z "$dir" ]; then
+        die "Directory path required"
+    elif [ ! -d "$dir" ]; then
+        die "Directory '$dir' does not exist"
+    fi
+    cd "$dir" || exit 1
+    make clean all
+}
+
+case "$1" in
+    help|--help|-h)
+        help
+        exit 0
+        ;;
+    check-verilator)
+        if command -v verilator >/dev/null 2>&1; then
+            verilator --version
+        else
+            echo "Error: Verilator not found"
+            exit 1
+        fi
+        ;;
+    verilator-example)
+        run_make_in_dir "$2"
+        ;;
+    change-verilator)
+        version="$2"
+        if [ -z "$version" ]; then
+            die "Version required for change-verilator"
+        fi
+        echo "Changing Verilator to version $version (installation logic not implemented)"
+        # TODO: Implement version switching logic
+        ;;
+    c-compiler-version)
+        if command -v gcc >/dev/null 2>&1; then
+            echo "GCC Version:"
+            gcc --version | head -n 1
+        else
+            echo "Error: GCC not found"
+        fi
+        if command -v make >/dev/null 2>&1; then
+            echo -e "\nGNU Make Version:"
+            make --version | head -n 1
+        else
+            echo "Error: GNU Make not found"
+        fi
+        ;;
+    c-compiler-example)
+        run_make_in_dir "$2"
+        ;;
+    *)
+        echo "Error: Unknown command: $1"
+        help
+        exit 1
+        ;;
+esac
